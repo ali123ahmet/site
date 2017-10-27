7bdff8c11a73ebccdb1256cdbe38ac9169eda039
ali ahmet <ali123ahmet@hotmail.co.uk>
2017-10-27 07:35:30 +0100

    Lataset

--- site.php
+++ site.php
@@ -1,8 +1,11 @@
 <html>
 <?php
 $container = array("name"=>'Ali Ahmet',"module"=>'Programming in PHP',"grade"=>'MERIT');
+session_start();
 foreach($container as $key => $value){
-    echo $key . ' : ' . $value; 
+    echo $key . ' : ' . $value .'<br>';
+    $_SESSION[$key] = $value; 
 }
+
 ?>
 </html>
