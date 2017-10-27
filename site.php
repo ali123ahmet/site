e368ca35cce683a148f355c1ae388a0182a9898a
ali ahmet <ali123ahmet@hotmail.co.uk>
2017-10-27 07:14:53 +0100

    updated changes

---
+++ site.php
@@ -0,0 +1,8 @@
+<html>
+<?php
+$container = array("name"=>'Ali Ahmet',"module"=>'Programming in PHP',"grade"=>'MERIT');
+foreach($container as $key => $value){
+    echo $key . ' : ' . $value; 
+}
+?>
+</html>
